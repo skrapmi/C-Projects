c358d14be5a98b04d85bf158d244fd93622b0751
Timothy Parks <parkstimothyj@csu.fullerton.edu>
2019-07-27 20:00:31 -0700

    started class multiply string

---
+++ MultiplyStrings/multiStrings.cpp
@@ -0,0 +1,66 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+
+class Solution {
+public:
+    string multiply(string num1, string num2) { 
+        if(num1 == "0" || num2 == "0")
+            return "0";
+        int min_length, max_length;
+        if(num1.length() < num2.length()){
+            string temp = num2;
+            num2 = num1;
+            num1 = temp;         
+        }    
+        cout << num1 << " " << num2 << " Mult function" << endl;
+        max_length = num1.length() - 1;
+        min_length = num2.length() - 1;
+        string newNum = "";
+        int zero = 0;
+        for(int y = min_length ; y >= 0; y--, zero++){
+            for(int x = max_length; x >= 0; x--){
+                int total = multiTwo( int(num1.at(x)) - 48 , int(num2.at(y)) - 48 );
+                newNum = addStrings(total, zero, newNum);
+                
+            }
+        }
+        return newNum;
+    }
+    
+    int multiTwo(int num1, int num2){
+        cout << num1 << " " << num2 << " Two Function" << endl;
+        return int(num1) * int(num2);    
+    }
+    
+    string addStrings(int twoNumTotal, int multiZero, string num1){
+        cout << "twoNumTotal = " << twoNumTotal << endl;
+        cout << "multiZero = " << multiZero << endl;
+        cout << "num1 = " << num1 << endl;
+        string num2 = "";
+        num2.insert(num2.end(), multiZero, '0');
+        num2.insert(0, to_string(twoNumTotal));
+        
+        if(num2.length() > num1.length()){
+            string temp = num1;
+            num1 = num2;
+            num2 = temp;         
+        }
+        
+        
+        
+        
+        return num1; //newTotal;
+    }
+    
+};
+
+int main(){
+    
+    Solution ans;
+    cout << ans.multiply("10","3");
+    cout << " done\n";
+    return 0;
+}
